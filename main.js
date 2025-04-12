b2fb0274026d9ca57fe9b73232bdd50d8c48ef26
Theon87 <theonfrancis17@gmail.com>
2025-04-11 22:12:14 -0400

    added html references to main.js

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
-// Logic here 
+const screenMemory = document.getElementById("memory");
+const screenMainInput = document.getElementById("mainInput");
+const memoryValue = document.getElementById("memoryVal");
+const mainVal = document.getElementById("mainVal");
+const clear = document.getElementById("clear");
+const clearLastEntry = document.getElementById("clearLastEntry");
+const backspace = document.getElementById("backspace");
+const divide = document.getElementById("divide");
+const multiply = document.getElementById("multiply");
+const subtract = document.getElementById("subtract");
+const add = document.getElementById("add");
+const one = document.getElementById("one");
+const two = document.getElementById("two");
+const three = document.getElementById("four");
+const four = document.getElementById("four");
+const five = document.getElementById("five");
+const six = document.getElementById("six");
+const seven = document.getElementById("seven");
+const eight = document.getElementById("eight");
+const nine = document.getElementById("nine");
+const decimalPoint = document.getElementById("decimalPoint");
+const positiveNegative = document.getElementById("positiveNegative");
+const percent = document.getElementById("percent");
+const enter = document.getElementById("enter");
+
+one.addEventListener("click", function () {
+  mainVal.innerHTML = 1;
+});
