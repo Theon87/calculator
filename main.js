b226ae3c562961129aa6e17165a1223cdcf4afd4
Theon87 <theonfrancis17@gmail.com>
2025-04-12 22:50:30 -0400

    added logic to digits, decimal point, plus/minus, backspace, and clear buttons

--- main.js
+++ main.js
@@ -11,18 +11,113 @@ const subtract = document.getElementById("subtract");
 const add = document.getElementById("add");
 const one = document.getElementById("one");
 const two = document.getElementById("two");
-const three = document.getElementById("four");
+const three = document.getElementById("three");
 const four = document.getElementById("four");
 const five = document.getElementById("five");
 const six = document.getElementById("six");
 const seven = document.getElementById("seven");
 const eight = document.getElementById("eight");
 const nine = document.getElementById("nine");
+const zero = document.getElementById("zero");
 const decimalPoint = document.getElementById("decimalPoint");
 const positiveNegative = document.getElementById("positiveNegative");
 const percent = document.getElementById("percent");
 const enter = document.getElementById("enter");
 
-one.addEventListener("click", function () {
-  mainVal.innerHTML = 1;
+let mainDisplay = [0];
+console.log(mainDisplay);
+
+mainVal.innerHTML = mainDisplay;
+
+clear.addEventListener("click", function () {
+  mainDisplay = [];
+  console.log(mainDisplay);
 });
+
+backspace.addEventListener("click", function () {
+  mainDisplay.pop();
+  display();
+  console.log(mainDisplay);
+});
+
+one.addEventListener("click", function () {
+  mainDisplay.push(1);
+  display();
+  console.log(mainDisplay);
+});
+
+two.addEventListener("click", function () {
+  mainDisplay.push(2);
+  display();
+  console.log(mainDisplay);
+});
+
+three.addEventListener("click", function () {
+  mainDisplay.push(3);
+  display();
+  console.log(mainDisplay);
+});
+
+four.addEventListener("click", function () {
+  mainDisplay.push(4);
+  display();
+  console.log(mainDisplay);
+});
+
+five.addEventListener("click", function () {
+  mainDisplay.push(5);
+  display();
+  console.log(mainDisplay);
+});
+
+six.addEventListener("click", function () {
+  mainDisplay.push(6);
+  display();
+  console.log(mainDisplay);
+});
+
+seven.addEventListener("click", function () {
+  mainDisplay.push(7);
+  display();
+  console.log(mainDisplay);
+});
+
+eight.addEventListener("click", function () {
+  mainDisplay.push(8);
+  display();
+  console.log(mainDisplay);
+});
+
+nine.addEventListener("click", function () {
+  mainDisplay.push(9);
+  display();
+  console.log(mainDisplay);
+});
+
+zero.addEventListener("click", function () {
+  mainDisplay.push(0);
+  display();
+  console.log(mainDisplay);
+});
+
+decimalPoint.addEventListener("click", function () {
+  mainDisplay.push(".");
+  display();
+  console.log(mainDisplay);
+});
+
+positiveNegative.addEventListener("click", function () {
+  mainDisplay.push("-");
+  display();
+  console.log(mainDisplay);
+});
+
+function display() {
+  if (mainDisplay.length > 0) {
+    for (i = 0; i < mainDisplay.length; i++) {
+      mainVal.innerHTML = mainDisplay[i];
+    }
+  } else {
+    mainVal.innerHTML = 0;
+  }
+}
